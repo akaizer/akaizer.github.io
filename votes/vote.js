1b450b44f9aff642239bbfbc60838a4c402b6e45
Andrew Kaizer <akaizer@indiana.edu>
2019-05-19 15:13:39 -0400

    testing

--- votes/vote.js
+++ votes/vote.js
@@ -1,7 +1,7 @@
 document.getElementById("load").onclick = async function () {
   let url = document.getElementById("url").value;
   console.log(url);
-  $.ajax(url, {type: "GET"}, function (data, status) {
+  $.ajax(url, {type: "GET", dataType: "jsonp"}, function (data, status) {
     console.log(data, status);
   })
 };
